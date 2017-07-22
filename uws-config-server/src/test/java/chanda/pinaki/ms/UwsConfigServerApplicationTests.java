25e5d222971de2108f2e31c590922858f78ab338
ADMIN <ADMIN@192.168.0.2>
2017-07-22 22:50:12 +0530

    Initial Commit - Config Server

---
+++ uws-config-server/src/test/java/chanda/pinaki/ms/UwsConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package chanda.pinaki.ms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UwsConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
