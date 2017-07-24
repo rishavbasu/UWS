8758b67e8b472ef12e6c700699c5aa0643e47035
Rishav <Rishav@192.168.1.2>
2017-07-25 00:08:10 +0530

    Multirow file upload Signed-off-by: Rishav <Rishav@192.168.1.2>

---
+++ product-entry-service/src/main/java/org/rishav/Product.java
@@ -0,0 +1,23 @@
+
+package org.rishav;
+
+public class Product {
+	private String	description;
+	private byte[]	image;
+
+	public final String getDescription() {
+		return this.description;
+	}
+
+	public final void setDescription(String description) {
+		this.description = description;
+	}
+
+	public final byte[] getImage() {
+		return this.image;
+	}
+
+	public final void setImage(byte[] image) {
+		this.image = image;
+	}
+}
---
+++ product-entry-service/src/main/java/org/rishav/ProductEntryForm.java
@@ -0,0 +1,16 @@
+package org.rishav;
+import java.util.List;
+
+public class ProductEntryForm {
+
+	private List<Product> products;
+
+	public final List<Product> getProducts() {
+		return this.products;
+	}
+
+	public final void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+}
