9ce128602d5f3a4a93c84a32a9b240dd8dda3265
rishavbasu <rrishavbasu@yahoo.co.in>
2020-09-02 18:32:40 +0530

    Update ProductEntryController.java

--- product-entry-service/src/main/java/com/uws/productentry/controller/ProductEntryController.java
+++ product-entry-service/src/main/java/com/uws/productentry/controller/ProductEntryController.java
@@ -41,4 +41,9 @@ public class ProductEntryController {
 	public Product fetchProductById(@PathVariable String productId) {
 		return productEntryService.fetchProductById(productId);
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public Product fetchProductById() {
+		return productEntryService.getAllProducts();
+	}
 }
